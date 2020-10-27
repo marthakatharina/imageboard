1c9bf5ca76d532f5cc70e7962a47ab0f4e1fdaa9
Marta Wlusek <wlusek.marta@gmail.com>
2020-10-27 12:57:31 +0100

    Imageboard part 1 and image upload completed

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
+const multer = require("multer"); // multer defines where to save files
+const uidSafe = require("uid-safe"); // encodes file name
+const path = require("path"); // grabs extention (jpg)
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 app.use(express.static("public"));
 
-app.listen(8080, () => console.log("image-board up and running"));
+app.get("/images", (req, res) => {
+    db.getImages()
+        .then(({ rows }) => {
+            console.log("rows: ", rows);
+            res.json(rows);
+        })
+        .catch((err) => {
+            console.log("error in getImages: ", err);
+        });
+});
+
+app.post("/upload", uploader.single("file"), function (req, res) {
+    // If nothing went wrong the file is already in the uploads directory
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
+});
+
+app.listen(8080, () => console.log("Imageboard up and running"));
+
+// image should be less than 2MB
+//upload.single("file") acts as middleware
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,96 @@
-console.log("sanity check");
+(function () {
+    new Vue({
+        el: "#main",
+        data: {
+            images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
+        },
+        mounted: function () {
+            let self = this;
+            axios.get("/images").then(function (response) {
+                self.images = response.data;
+            });
+        },
+        methods: {
+            handleClick: function (e) {
+                e.preventDefault();
+                console.log("this: ", this); // this access everything inside data:{}
+
+                var formData = new FormData(); // formData sends stuff to the server axios
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+
+                // no console.log needed here, it will show an empty obj
+
+                axios
+                    .post("/upload", formData)
+                    .then(function (response) {
+                        console.log("response from POST / upload: ", response);
+                    })
+                    .catch(function (err) {
+                        console.log("err in POST /upload: ", err);
+                    });
+            },
+
+            handleChange: function (e) {
+                console.log("handleChange is running!!!!");
+                console.log("file: ", e.target.files[0]);
+                this.file = e.target.files[0];
+            },
+        },
+    });
+})();
+
+// console.log("sanity check");
+
+// new Vue({
+//     el: "#main",
+//     data: {
+//         name: "Pimento",
+//         seen: true,
+//         cities: [
+//             // {
+//             //     name: "Berlin",
+//             //     country: "Germany",
+//             // },
+//             // {
+//             //     name: "Warsaw",
+//             //     country: "Poland",
+//             // },
+//         ],
+//     },
+//     mounted: function () {
+//         console.log("mounted");
+//         console.log("this.name", this.name);
+//         console.log("this.cities", this.cities);
+//         // this.cities = [
+//         //     { name: "Berlin", country: "Germany" },
+//         //     { name: "Warsaw", country: "Poland" },
+//         // ];
+
+//         //axios is e library for makin db requests
+//         var self = this; // self used instead of this
+
+//         axios.get("/cities").then(function (response) {
+//             console.log("response: ", response);
+//             console.log("self: ", self);
+//             console.log("this.cities inside then: ", this.cities);
+//             console.log("this inside then: ", this);
+
+//             // this.cities = response.date;
+//             // this inside then (nested fn) refers to global object so it has to be stored in var
+//             self.cities = response.data;
+//         });
+//     },
+
+//     methods: {
+//         myFancyMethod: function (cityName) {
+//             console.log("clicked", cityName);
+//         },
+//     },
+// });
